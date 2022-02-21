90a23d013db173f7bde45bf0cfc4f6d403eb1cc6
git-azeez <87197563+git-azeez@users.noreply.github.com>
2022-02-21 12:03:57 +0530

    Update main.tf
    added a new code in the ending which creates VPC connector an NAT gateway aswell

--- main.tf
+++ main.tf
@@ -9,17 +9,40 @@
 
 
  // 1. Creation of cloud run service
-resource "google_cloud_run_service" "run" {
-  
+resource "google_cloud_run_service" "dev" {
+
+ 
   for_each = var.service-image
-  name     = each.key
+  name  = each.key
+  
   location = "asia-south1"
 
-  template {
+   template {
     spec {
-        service_account_name = "cloud-run-az@searce-msp-gcp.iam.gserviceaccount.com" 
+       service_account_name = "cloud-run-az@searce-msp-gcp.iam.gserviceaccount.com" 
+            
+    
       containers {
+    
         image = each.value
+        resources {
+          limits = {
+            cpu = "1000m"
+            memory = "512M"
+          }
+        }
+        
+      }
+    }
+    metadata {
+      annotations = {
+        # limit scale up to prevent any cost blow outs!
+        "autoscaling.knative.dev/maxScale" = "5"
+        "autoscaling.knative.dev/minScale" = "0"
+        # use the VPC Connector above
+        "run.googleapis.com/vpc-access-connector" ="az-vpc-connector"
+        # all egress from the service should go through the VPC Connector
+        "run.googleapis.com/vpc-access-egress" = "all"
       }
     }
   }
@@ -71,3 +94,72 @@ resource "google_compute_backend_service" "backend" {
     group = google_compute_region_network_endpoint_group.endpoint[each.key].id
    }
 }  
+
+
+
+
+////////////////////////
+
+# creation of cloudrun + vpc connector + NAT gateway
+
+# provider "google" {
+#   version = "~> 3.46.0"
+#   region  = var.region
+#   project = var.project_id
+# }
+
+# resource "google_vpc_access_connector" "connector" {
+#   name          = "vpcconn"
+#   region        = var.region
+#   # e.g. "10.8.0.0/28"
+#   ip_cidr_range = var.serverless_vpc_conn_cidr
+#   network       = var.network
+# }
+
+# resource "google_compute_router" "router" {
+#   name    = "router"
+#   project = var.project_id
+#   region  = var.region
+#   network = var.network
+# }
+
+# resource "google_compute_router_nat" "nat" {
+#   name                               = "nat"
+#   project                            = var.project_id
+#   region                             = var.region
+#   router                             = google_compute_router.router.name
+#   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+#   nat_ip_allocate_option             = "AUTO_ONLY"
+# }
+
+# resource "google_cloud_run_service" "gcr_service" {
+#   name     = "mygcrservice"
+#   location = var.region
+
+#   template {
+#     spec {
+#       containers {
+#         image = var.myservice_image
+#         resources {
+#           limits = {
+#             memory = "512M"
+#           }
+#         }
+#       }
+#       # the service itself uses this SA to call other GCP APIs etc
+#       service_account_name = var.myservice_runtime_sa
+#     }
+
+#     metadata {
+#       annotations = {
+#         # limit scale up to prevent any cost blow outs!
+#         "autoscaling.knative.dev/maxScale" = "5"
+#         # use the VPC Connector above
+#         "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.connector.name
+#         # all egress from the service should go through the VPC Connector
+#         "run.googleapis.com/vpc-access-egress" = "all"
+#       }
+#     }
+#   }
+#   autogenerate_revision_name = true
+# }
